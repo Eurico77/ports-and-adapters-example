1240090a795d235d5a64e49840fcd2481d55232d
eurico77 <euricomagalhaes61@gmail.com>
2024-06-13 14:10:21 -0300

    shold create an user with password encrypted

--- tests/RegisterUser.spec.ts
+++ tests/RegisterUser.spec.ts
@@ -1,14 +1,32 @@
-import { DatabaseInterface } from '@/app/ports/Bd';
-import RegisterUser from '@/app/users/RegisterUser';
+import HashEncrypt from '../src/adapters/HashEncrypt';
+import { DatabaseInterface } from '../src/core/ports/Bd';
+import RegisterUser from '../src/core/usecases/RegisterUser';
 
-test('Deve registrar um usuário', () => {
-  const database: DatabaseInterface = {
-    insert(data) {
-      const users = [];
-      users.push(data);
-    },
-  };
-  const createUseUseCase = new RegisterUser(database);
-  const user = createUseUseCase.execute('Eurico', 'eurico@mail.com', '1234');
-  expect(user).toHaveProperty('id');
+describe('Register User Use Case', () => {
+  test('Deve registrar um usuário', () => {
+    const database: DatabaseInterface = {
+      insert(data) {
+        const users = [];
+        users.push(data);
+      },
+    };
+    const bcryptHashAdapter = new HashEncrypt();
+    const createUseUseCase = new RegisterUser(database, bcryptHashAdapter);
+    const user = createUseUseCase.execute('Eurico', 'eurico@mail.com', '1234');
+    expect(user).toHaveProperty('id');
+  });
+
+  test('Deve registrar um com senha criptografada', () => {
+    const database: DatabaseInterface = {
+      insert(data) {
+        const users = [];
+        users.push(data);
+      },
+    };
+    const bcryptHashAdapter = new HashEncrypt();
+    const createUseUseCase = new RegisterUser(database, bcryptHashAdapter);
+    const user = createUseUseCase.execute('Eurico', 'eurico@mail.com', '1234');
+    const passIsHashed = bcryptHashAdapter.compare('1234', user.pass);
+    expect(passIsHashed).toBe(true);
+  });
 });
